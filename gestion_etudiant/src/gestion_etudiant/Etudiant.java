2f03ac033d191419494af659bfc0b24f88a5a617
FatihaKHALIFA <FatihaKHALIFA@10.21.254.110>
2022-04-15 16:55:04 +0200

    creation du constructeur et definition de la methode tostring

--- gestion_etudiant/src/gestion_etudiant/Etudiant.java
+++ gestion_etudiant/src/gestion_etudiant/Etudiant.java
@@ -4,5 +4,18 @@ public class Etudiant {
 	private int id;
 	private String nom ;
 	private double note;
+	public Etudiant() {
+		this.id= id;
+		this.nom=nom;
+		this.note=10;
+		public String toString()
+		{
+			return "("+this.nom+":"+this.note+")" ;
+		}
+		
+		
+		
+		
+	}
 
 }
